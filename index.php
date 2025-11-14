15e5e378ea508164b691d07dd77ba14295183933
BurdilesNicolas <burdilescarroandres.epet20@gmail.com>
2025-11-14 09:52:10 -0300

    Cambiando detalles para el correcto funcionamiento del CRUD

---
+++ index.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Guitar Hero</title>
+  <link rel="stylesheet" href="CSS/style.css">
+  <link rel="stylesheet" href="CSS/fijo.css">
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap');
+</style>
+</head>
+<body>
+  <header>
+    <div class="logo">
+      <h1>GUITAR<span>HERO</span></h1>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="galeria.html">Galeria</a></li>
+        <li><a href="crud_reviews/reviews.php">Reseñas</a></li>
+        <li><a href="contactos.html">Contacto</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  
+
+  <section class="buscador">
+    <input type="text" placeholder=" Buscar guitarras...">
+  </section>
+
+  <main>
+    <section class="destacados">
+  <h2>Destacados</h2>
+
+  <div class="filtros">
+    <button class="activo">Novedades</button>
+    <button>Stratocaster</button>
+    <button>Telecaster</button>
+    <button>Acoustic</button>
+    <button>New Headwear</button>
+  </div>
+
+  <div class="galeria">
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="SG Standard 61 Stop Bar">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>SG Standard '61 Stop Bar, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€1.999</p>
+  <a href="guitarra.php?id=1"><button class="ver-mas">Ver más</button></a>
+
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Firebird Platypus">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Firebird Platypus, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Explorer 80s">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Explorer 80s, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Flying V 70s">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Flying V 70s, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Flying V 70s">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Flying V 70s, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+    <section class="destacados">
+  <h2>Stratocasters</h2>
+
+  <div class="galeria">
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="SG Standard 61 Stop Bar">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>SG Standard '61 Stop Bar, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€1.999</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Firebird Platypus">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Firebird Platypus, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Explorer 80s">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Explorer 80s, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Flying V 70s">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Flying V 70s, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Flying V 70s">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Flying V 70s, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+<section class="destacados">
+  <h2>Telecasters</h2>
+
+  <div class="galeria">
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="SG Standard 61 Stop Bar">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>SG Standard '61 Stop Bar, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€1.999</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Firebird Platypus">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Firebird Platypus, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Explorer 80s">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Explorer 80s, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Flying V 70s">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Flying V 70s, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Flying V 70s">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Flying V 70s, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+<section class="destacados">
+  <h2>Acoustics</h2>
+
+  <div class="galeria">
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="SG Standard 61 Stop Bar">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>SG Standard '61 Stop Bar, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€1.999</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Firebird Platypus">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Firebird Platypus, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Explorer 80s">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Explorer 80s, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Flying V 70s">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Flying V 70s, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+    <div class="guitarra">
+      <img src="img/SG_Historic_SGSR64VOCHNM1.webp" alt="Flying V 70s">
+      <div class="info">
+        <p class="etiqueta">Novedad | Producto exclusivo</p>
+        <h3>Flying V 70s, Exclusive</h3>
+        <p class="marca">Gibson</p>
+        <p class="color">British Racing Green</p>
+        <p class="precio">€2.499</p>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+  </main>
+  <footer>
+    <p>© 2025 GuitarHero| Dulce de leche</p>
+  </footer>
+</body>
+</html>
